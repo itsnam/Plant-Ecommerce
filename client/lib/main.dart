b425dc56b79d436e53f2d2e092d3e817c5f0a200
p741852963357 <phamphuc0932@gmail.com>
2024-01-11 21:03:36 +0700

    init

---
+++ client/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Poppins'
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Plantial',
+          style: TextStyle(fontWeight: FontWeight.w700),
+        ),
+      ),
+      body: const Center(
+        child: Text('Poppins'),
+      ),
+    );
+  }
+}
+
+
