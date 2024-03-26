92377307506433a3a038808aff2488c0c99c1a8a
p741852963357 <phamphuc0932@gmail.com>
2024-03-26 16:12:49 +0700

    m

---
+++ admin/src/pages/orders/api/getOrders.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+import { API_URL } from "../../../config/index.js";
+
+export const getOrders = async () => {
+  return await axios.get(API_URL + '/orders').then(res => res.data);
+}
